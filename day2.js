f82d69b4db2ee218612862cbd3a485292e952105
Anas Shaikh <anasshaikh7670@gmail.com>
2026-05-26 21:39:56 +0530

    Day 2: Data types, operators, type conversion

---
+++ day2.js
@@ -0,0 +1,7 @@
+let celsius = 37;
+let fahrenheit = (celsius * 9/5) + 32;
+console.log(`${celsius}°C = ${fahrenheit}°F`); 
+
+let km = 10;
+let miles = (km * 0.621371).toFixed(2);
+console.log(`${km}km = ${miles} miles`); 
